d4fbc6721fbeca5cb1fe523fcab496c45b3e4ff0
Clement Nkamanyi <c.nkamanyi@clenkasoft.com>
2025-08-30 12:07:04 +0200

    Added docker support Removed DesignTimeDbContextFactory

---
+++ src/backend/Farmundo.Demo.Infrastructure/Persistence/Migrations/20250830094439_RemovedDesignTimeFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Farmundo.Demo.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedDesignTimeFactory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
